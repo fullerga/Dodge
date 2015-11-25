18d6edc8e4821c4dbe8a34251d5a6d73b9ef7a36
oakesja <jacoboakes92@gmail.com>
2015-11-24 21:23:29 -0500

    added health powerup

---
+++ Assets/Scripts/Powerups/HealthPowerup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class HealthPowerup : PowerUp
+{
+    public string Tag { get { return "health"; } }
+    public string TextForAnimation { get { return "RESTORE HEALTH"; } }
+    public Action StartPowerup
+    {
+        get
+        {
+            return () =>
+            {
+                PowerUpManager.HealthBar.IncHealth();
+                PowerUpManager.Reset();
+            };
+        }
+    }
+}
