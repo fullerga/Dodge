bf37f752faea5e948696e4318fc5d2a95a03cfb1
fullerga <fullerga@rose-hulman.edu>
2016-01-02 02:38:33 -0800

    changed level1 and level8

--- Assets/Scripts/Spawners/Level1.cs
+++ Assets/Scripts/Spawners/Level1.cs
@@ -6,9 +6,9 @@ public class Level1 : RandomSpawner
 	protected override void Start ()
     {
         enemies = new ArrayList();
-        for(int i=0; i<1; i++)
+        for(int i=0; i<10; i++)
         {
-            enemies.Add("spikeBall");
+            enemies.Add("straightSlow");
         }
     }
 }
